a72f7e933ef3ff31ff4e1869f3421a8ee7b6aa3b
Mateus Moreira <moreiramateusjp@gmail.com>
2025-09-13 13:42:38 -0300

    segundo commit do codigo do projeto da fabrica de software

--- wsfrontend.fabrica25.2/src/app/componentes/rodape/footer.jsx
+++ wsfrontend.fabrica25.2/src/app/componentes/rodape/footer.jsx
@@ -6,7 +6,7 @@ export default function Rodape() {
     <footer>
       <div className="containers">
         <div className="caixa">
-          <p>&copy; 2025 Pokémon GO Fan Site. Todos os direitos reservados.</p>
+          <p>&copy; 2025 Pokémon GO Fan Site.</p>
         </div>
       </div>
     </footer>
--- wsfrontend.fabrica25.2/src/app/page.js
+++ wsfrontend.fabrica25.2/src/app/page.js
@@ -1,51 +1,51 @@
-"use client";
-import { useState, useEffect } from "react";
-import Header from "./componentes/cabecalho/header.jsx";
-import PokemonCard from "./componentes/cards/cards.jsx";
-import Footer from "./componentes/rodape/footer.jsx"
-import BotaoGrid from "./componentes/botaoMudarGrid/mudaGrid.jsx"
-import "./globals.css"
+  "use client";
+  import { useState, useEffect } from "react";
+  import Header from "./componentes/cabecalho/header.jsx";
+  import PokemonCard from "./componentes/cards/cards.jsx";
+  import Footer from "./componentes/rodape/footer.jsx"
+  import BotaoGrid from "./componentes/botaoMudarGrid/mudaGrid.jsx"
+  import "./globals.css"
 
 
-export default function Home() {
+  export default function Home() {
 
-  const [pokemons, setPokemons] = useState([])
-  const [pesquisa, setPesquisa] = useState("")
-  const [layoutAlternativo, setLayoutAlternativo] = useState(false)
+    const [pokemons, setPokemons] = useState([])
+    const [pesquisa, setPesquisa] = useState("")
+    const [layoutAlternativo, setLayoutAlternativo] = useState(false)
 
-  useEffect(()=>{
-    const buscarPokemon = async() =>{
+    useEffect(()=>{
+      const buscarPokemon = async() =>{
 
-      const resposta = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
-      const dados = await resposta.json()
+        const resposta = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
+        const dados = await resposta.json()
 
-      const detalhes = []
+        const detalhes = []
 
-      for(const p of dados.results){
-        const respostaDetalhada = await fetch(p.url)
-        const pokemonDetalhado = await respostaDetalhada.json()
-        detalhes.push(pokemonDetalhado)
-      }
-      setPokemons(detalhes)
-    } 
+        for(const p of dados.results){
+          const respostaDetalhada = await fetch(p.url)
+          const pokemonDetalhado = await respostaDetalhada.json()
+          detalhes.push(pokemonDetalhado)
+        }
+        setPokemons(detalhes)
+      } 
 
-    buscarPokemon()
-  },[])
+      buscarPokemon()
+    },[])
 
-  const filtrarPokemon = pokemons.filter((p)=>
-    p.name.toLowerCase().includes(pesquisa.toLowerCase())
-  )
+    const filtrarPokemon = pokemons.filter((p)=>
+      p.name.toLowerCase().includes(pesquisa.toLowerCase())
+    )
 
-  return (
-    <>
-      <Header pesquisa={pesquisa} setPesquisa={setPesquisa} />
-      <BotaoGrid layoutAlternativo={layoutAlternativo} setLayoutAlternativo={setLayoutAlternativo}/>
-      <main className={`container-de-carta ${layoutAlternativo ? "grid-alternativo" : ""}`}>
-        {filtrarPokemon.map((p)=>(
-          <PokemonCard pokemon={p} key={p.id} layoutAlternativo={layoutAlternativo}/>
-        ))}
-      </main>
-      <Footer/>
-    </>
-  );
-}
+    return (
+      <>
+        <Header pesquisa={pesquisa} setPesquisa={setPesquisa} />
+        <BotaoGrid layoutAlternativo={layoutAlternativo} setLayoutAlternativo={setLayoutAlternativo}/>
+        <main className={`container-de-carta ${layoutAlternativo ? "grid-alternativo" : ""}`}>
+          {filtrarPokemon.map((p)=>(
+            <PokemonCard pokemon={p} key={p.id} layoutAlternativo={layoutAlternativo}/>
+          ))}
+        </main>
+        <Footer/>
+      </>
+    );
+  }
